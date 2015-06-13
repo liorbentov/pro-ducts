46e5d088ddb16fdd4f23f2c77131e99e4f49d178
Lior Bentov <liorbentov@gmail.com>
2015-06-13 21:25:39 +0300

    add photos to podium

--- public/js/controllers/product.js
+++ public/js/controllers/product.js
@@ -7,14 +7,78 @@ angular.module('proDucts.controllers').controller('productController', ['$scope'
   //var selectedItem = 0;
 	productsService.getProducts(function(results){$scope.products = results});
 
-  $scope.chosenFeatures = featuresService.getChosenFeatures();
-
-	$scope.features = null;
-
-	var promise = featuresService.getFeatures();
-	promise.then(function(results){
-		$scope.features = results;
+	productsService.setProductImage(0, function(results){
+		$scope.products = results;
 	});
+
+	productsService.setProductImage(1, function(results){
+		$scope.products = results;
+	});
+
+	productsService.setProductImage(2, function(results){
+		$scope.products = results;
+	});
+
+	$scope.findFeatureInArray = function(feature, array) {
+		if (feature) {
+			for (var i = 0; i < array.length; i++) {
+				if (array[i].featureId == feature) {
+					return i;
+				}
+			};
+		}
+		return -1;
+	};
+
+	$scope.findValueInArray = function(value, array) {
+		if (value) {
+			for (var i = 0; i < array.length; i++) {
+				if (array[i].key == value) {
+					return i;
+				}
+			};
+		}
+		return -1;
+	}
+
+	$scope.selectedItemChanged = function() {
+		// We need to set the list of the features so that we can see all the chosen features
+		// and those that have grades
+		var promise = featuresService.getFeaturesAsArray();
+		promise.then(function(results){
+			$scope.features = results;
+			// Get chosenFeatures
+			$scope.chosenFeatures = featuresService.getChosenFeatures();
+
+			// For each feature
+			$scope.features.forEach(function(feature){
+				// If it is in the chosenFeatures array, add Bold attribute
+				if ($scope.findValueInArray(feature.key, $scope.chosenFeatures) != -1) {
+					feature.bolder = true;
+				}
+				else {
+					feature.bolder = false;
+				}
+
+				var index = $scope.findFeatureInArray(feature.key, $scope.products[selectedItem].features);
+				// If it have grade, add it - if not, set grade to zero
+				if (index != -1) {
+					feature.grade = ($scope.products[selectedItem].features[index].grade.toPrecision()*100).toFixed();
+					feature.caption = ($scope.products[selectedItem].features[index].grade.toPrecision()*100).toFixed();
+					feature.opacity = 1;
+				}
+				else {
+					feature.grade = 0;
+					feature.caption = "אין ציון";
+					feature.opacity = 0.4;
+				}
+			});
+		});
+
+
+	}
+
+		$scope.selectedItemChanged();
 
 	$scope.filterProduct = function (item){
 	    if (item.Name.toUpperCase().indexOf($scope.productsFilter.toUpperCase())!=-1 /*|| item.Grade.toString().indexOf($scope.productsFilter)!=-1*/) {
@@ -35,6 +99,7 @@ angular.module('proDucts.controllers').controller('productController', ['$scope'
     $scope.displayProduct = function(productIndex) {
           $scope.selectedItem = productIndex;
           selectedItem = productIndex;
+					$scope.selectedItemChanged();
     };
 
   	$scope.productRight = function () {
@@ -48,6 +113,7 @@ angular.module('proDucts.controllers').controller('productController', ['$scope'
 
   			selectedItem--;
   			$scope.selectedItem--;
+				$scope.selectedItemChanged();
   		}
   	}
 
@@ -62,6 +128,8 @@ angular.module('proDucts.controllers').controller('productController', ['$scope'
 
   			selectedItem++;
   			$scope.selectedItem++;
+				$scope.selectedItemChanged();
   		}
   	}
+
 }]);
